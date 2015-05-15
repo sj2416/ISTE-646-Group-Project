907c2814252b59338a43c8fe5b1aa4fdcb0d0959
Sweta <sweta.jhv@gmail.com>
2015-05-15 16:22:32 -0400

    added appropriate php files for login/registration

---
+++ php/adminRegister.php
@@ -0,0 +1,5 @@
+<?php
+    echo 'This PHP will register professor and redirect to login.html'; // This is a one-line c++ style comment
+    /* Redirect browser */
+	header('Location: login.html');
+?>
